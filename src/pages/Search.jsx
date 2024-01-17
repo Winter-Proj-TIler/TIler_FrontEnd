ca724966588db15cca68637d488eece2dc078371
six-standard <dbrrl1127@gmail.com>
2024-01-17 19:09:13 +0900

    chore: 검색 페이지 파일 추가

---
+++ src/pages/Search.jsx
@@ -0,0 +1,3 @@
+export const Search = () => {
+  return <h1 className="dark:text-neutral-100">test</h1>;
+};
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import * as _ from "../pages";
 import { Layout } from "./Layout";
+import * as _ from "../pages";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route element={<_.Home />} path="/" />
+        <Route element={<Layout />} path="/">
+          <Route element={<_.Home />} path="" />
+        </Route>
+        <Route element={<Layout back />} path="/">
+          <Route element={<_.Search />} path="search" />
         </Route>
       </Routes>
     </BrowserRouter>
