b1923725ab55d040d82f0fc723dc55516caf8ef1
six-standard <dbrrl1127@gmail.com>
2024-01-18 15:36:54 +0900

    chore: 드롭다운 컴포넌트 제작 밖을 클릭해도 창이 닫히지 않는 문제 해결해야 함

---
+++ src/components/common/Dropdown.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export default function Dropdown({ value, children }) {
+  const [opened, setOpened] = useState(false);
+
+  const handleOpen = () => {
+    setOpened((prev) => !prev);
+  };
+
+  return (
+    <div className="select-none relative" onClick={handleOpen}>
+      <div className="cursor-pointer">{value}</div>
+      <div
+        id="dropdown"
+        className={`${
+          opened ? "flex" : "hidden"
+        } flex-col min-w-32 p-3 gap-3 mt-4 bg-neutral-100 dark:bg-neutral-900 rounded-md absolute left-[-100%] cursor-pointer dark:text-neutral-100`}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
