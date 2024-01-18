70949ea809881e312e0f642a8d06ef1b9ed8ed79
six-standard <dbrrl1127@gmail.com>
2024-01-18 16:03:49 +0900

    chore: 자동 다크모드 지원 추가

--- src/hooks/useDark.js
+++ src/hooks/useDark.js
@@ -1,12 +1,14 @@
 import { create } from "zustand";
 
 const useDark = create((set, get) => ({
-  dark: false,
+  dark: !!localStorage.getItem("dark"),
   setDark: () => {
     const html = document.querySelector("html");
     if (get().dark === false) {
+      localStorage.setItem("dark", true);
       html.classList = ["dark"];
     } else {
+      localStorage.removeItem("dark");
       html.classList = [];
     }
     set({
