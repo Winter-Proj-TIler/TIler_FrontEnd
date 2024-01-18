a5a5dae1773dd732dfee63238b59439a54547612
six-standard <dbrrl1127@gmail.com>
2024-01-18 15:38:22 +0900

    chore: 일부 이미지 추가

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,5 +6,18 @@ import Github from "./Logo/Github.png";
 import Search from "./Search.svg";
 import { ReactComponent as Comment } from "./Comment.svg";
 import { ReactComponent as Like } from "./Like.svg";
+import BackArrow from "./Back.svg";
+import Write from "./Write.svg";
 
-export { Dark, Light, Logo, Search, Comment, Like, Google, Github };
+export {
+  Dark,
+  Light,
+  Logo,
+  Search,
+  Comment,
+  Like,
+  Google,
+  Github,
+  BackArrow,
+  Write,
+};
