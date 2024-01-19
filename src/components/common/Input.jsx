035995ed45f1c5b741c152c1948fffcc133771f3
six-standard <dbrrlwns1127@gmail.com>
2024-01-19 22:53:54 +0900

    chore: Input 컴포넌트 스타일 프롭 추가

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,7 +1,7 @@
-export default function Input({ type, placeholder, id, action, width }) {
+export default function Input({ className, type, placeholder, id, action }) {
   return (
     <input
-      className="w-full p-2 bg-neutral-200 dark:bg-neutral-800 dark:placeholder:text-neutral-600 dark:text-neutral-50 rounded-md"
+      className={`${className} w-full p-2 bg-neutral-100 placeholder:text-neutral-400 dark:bg-neutral-800 dark:placeholder:text-neutral-600 dark:text-neutral-50 rounded-md`}
       placeholder={placeholder}
       onChange={action}
       type={type}
