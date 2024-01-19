9d42d0bb0ffcf761d97fa5dfeb6866211b0bc8d0
six-standard <dbrrlwns1127@gmail.com>
2024-01-19 22:54:12 +0900

    chore: Following 컴포넌트 기능 추가

--- src/components/follow/Following.jsx
+++ src/components/follow/Following.jsx
@@ -1,18 +1,20 @@
 import Button from "../common/Button";
 
-export default function Following() {
+export default function Following({ user }) {
+  const handleUnfollow = () => {
+    // React-Query mutation 코드 + 성공시 setQueryData
+  };
+
   return (
     <div className="flex justify-between items-center w-[68rem] gap-3">
       <div className="flex items-center gap-2">
-        <img src="/prf.png" />
+        <img src={user.profile} alt="" />
         <div>
-          <h1 className="text-2xl dark:text-neutral-100">Six-Standard</h1>
-          <h1 className="text-lg dark:text-neutral-100">
-            Lorem Ipsum Dollar one two three four five six standard
-          </h1>
+          <h1 className="text-2xl dark:text-neutral-100">{user.name}</h1>
+          <h1 className="text-lg dark:text-neutral-100">{user.inform}</h1>
         </div>
       </div>
-      <Button>언팔로우</Button>
+      <Button onClick={handleUnfollow}>언팔로우</Button>
     </div>
   );
 }
