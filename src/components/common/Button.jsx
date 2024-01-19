01653f636aa1a43b9aabdd0fba2ac8409bc135a7
six-standard <dbrrlwns1127@gmail.com>
2024-01-19 22:53:03 +0900

    chore: Button 컴포넌트 액션 추가

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,6 +1,9 @@
-export default function Button({ children }) {
+export default function Button({ children, onClick }) {
   return (
-    <button className="box-content px-6 py-1 rounded-2xl border-2 border-neutral-900 dark:border-neutral-100 dark:text-neutral-100">
+    <button
+      className="box-content px-6 py-1 rounded-2xl border-2 border-neutral-900 dark:border-neutral-100 dark:text-neutral-100"
+      onClick={onClick}
+    >
       {children}
     </button>
   );
