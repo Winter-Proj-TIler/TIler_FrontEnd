5654f9a995e66e9f4e65c80985a7ece02278a66c
six-standard <dbrrlwns1127@gmail.com>
2024-01-20 20:40:30 +0900

    chore: input 컴포넌트 기본값 추가

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,10 +1,18 @@
-export default function Input({ className, type, placeholder, id, action }) {
+export default function Input({
+  className,
+  type,
+  placeholder,
+  id,
+  value,
+  action,
+}) {
   return (
     <input
       className={`${className} w-full p-2 bg-neutral-100 placeholder:text-neutral-400 dark:bg-neutral-800 dark:placeholder:text-neutral-600 dark:text-neutral-50 rounded-md`}
       placeholder={placeholder}
       onChange={action}
       type={type}
+      value={value}
       id={id}
     />
   );
