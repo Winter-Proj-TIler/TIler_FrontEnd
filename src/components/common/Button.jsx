9e33d6a7f48775fd57f48322df12cef625f28c77
six-standard <dbrrlwns1127@gmail.com>
2024-01-20 20:40:12 +0900

    chore: 버튼 컴포넌트 스타일 변경

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,7 +1,7 @@
-export default function Button({ children, onClick }) {
+export default function Button({ children, onClick, className }) {
   return (
     <button
-      className="box-content px-6 py-1 rounded-2xl border-2 border-neutral-900 dark:border-neutral-100 dark:text-neutral-100"
+      className={`${className} flex gap-1 box-content px-2 rounded-lg border-[1px] border-neutral-900 dark:border-neutral-100 dark:text-neutral-100`}
       onClick={onClick}
     >
       {children}
