cddcf4e13d7ed9c38cd9bfe46a24e679c642bd79
six-standard <dbrrlwns1127@gmail.com>
2024-01-21 16:07:44 +0900

    chore: 태그 파일 위치 변경

--- src/components/detail/Tags.jsx
+++
@@ -1,7 +0,0 @@
-export default function Tags({ children }) {
-  return (
-    <div className="px-4 rounded-full border-[1px] text-sm border-neutral-950 dark:border-neutral-100">
-      {children}
-    </div>
-  );
-}
