83b266ed59f02aa49dae1d2b8ed51eb17a81030a
six-standard <dbrrlwns1127@gmail.com>
2024-02-25 12:47:01 +0900

    chore: api 코드 일부 수정

--- src/api/like.js
+++ src/api/like.js
@@ -6,6 +6,6 @@ export const like = async (postID) => {
   return await instance.post(`/${path}/${postID}`);
 };
 
-export const unLike = async (userID) => {
+export const unLike = async (postID) => {
   return await instance.delete(`/${path}/${postID}`);
 };
--- src/api/post.js
+++ src/api/post.js
@@ -2,22 +2,22 @@ import { instance } from ".";
 
 const path = "post";
 
-export const searchPost = async (sort, keyword) => {
-  return await instance.get(
-    `/${path}/search/keyword/${sort}?keyword=${keyword}`
-  );
-};
+export const searchPost = async (sort, keyword) =>
+  await instance.get(`/${path}/search/keyword/${sort}?keyword=${keyword}`);
 
-export const searchPostTag = async (tag) => {
-  return await instance.get(`/${path}/logout`);
-};
+export const searchPostTag = async (tag) =>
+  await instance.get(`/${path}/search/tag/${tag}/DESC`);
 
 export const uploadPost = async (data) => {
-  return await instance.post(`/${path}/`, data);
+  const item = {
+    ...data,
+    tags: data.tags.split("_"),
+  };
+  return await instance.post(`/${path}/`, item);
 };
 
 export const detailPost = async (postID) => {
-  return await instance.post(`/${path}/${postID}`);
+  return await instance.get(`/${path}/${postID}`);
 };
 
 export const updatePost = async (postID, data) => {
@@ -29,5 +29,5 @@ export const deletePost = async (postID) => {
 };
 
 export const uploadImg = async (file) => {
-  return await instance.post(`/${path}/${postID}`);
+  return await instance.post(`/${path}/img`, file);
 };
