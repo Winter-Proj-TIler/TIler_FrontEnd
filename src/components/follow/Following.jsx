beb02e7b936ae644b5324de1743f62fe9448b4b5
six-standard <dbrrlwns1127@gmail.com>
2024-02-25 12:47:55 +0900

    feat: 팔로우 관리 페이지 완성

--- src/components/follow/Following.jsx
+++ src/components/follow/Following.jsx
@@ -1,20 +1,30 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import Button from "../common/Button";
+import { queryKeys } from "../../utils/queryKeys";
+import { unFollow } from "../../api/follow";
 
 export default function Following({ user }) {
-  const handleUnfollow = () => {
-    // React-Query mutation 코드 + 성공시 setQueryData
-  };
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation({
+    mutationFn: () => unFollow(user.userId),
+    onSuccess: () => {
+      localStorage.removeItem(`follow${user.userId}`);
+      queryClient.invalidateQueries([queryKeys.follow, queryKeys.detail]);
+    },
+  });
 
   return (
     <div className="flex justify-between items-center w-[68rem] gap-3">
       <div className="flex items-center gap-2">
-        <img src={user.profile} alt="" />
+        <img src={user.profileImg} alt="" className="w-[100px] rounded-full" />
         <div>
-          <h1 className="text-2xl dark:text-neutral-100">{user.name}</h1>
-          <h1 className="text-lg dark:text-neutral-100">{user.inform}</h1>
+          <h1 className="text-2xl dark:text-neutral-100">{user.username}</h1>
+          <h1 className="text-lg dark:text-neutral-100">
+            {user.statusMsg ? user.statusMsg : "상태메세지 없음"}
+          </h1>
         </div>
       </div>
-      <Button onClick={handleUnfollow}>언팔로우</Button>
+      <Button onClick={mutate}>언팔로우</Button>
     </div>
   );
 }
--- src/pages/Follow.jsx
+++ src/pages/Follow.jsx
@@ -1,17 +1,25 @@
-import Dropdown from "../components/common/Dropdown";
+import { useQuery } from "@tanstack/react-query";
+import { followList } from "../api/follow";
 import Following from "../components/follow/Following";
+import { queryKeys } from "../utils/queryKeys";
+
+// 끝
 
 export const Follow = () => {
+  const { data } = useQuery({
+    queryKey: [queryKeys.follow],
+    queryFn: followList,
+    select: (res) => {
+      return res.data.list;
+    },
+  });
+
   return (
     <div className="flex flex-col items-center gap-3 w-[68rem]">
       <h1 className="self-start text-2xl font-bold">팔로잉 관리</h1>
-      <Following
-        user={{
-          profile: "/prf.png",
-          name: "six-standard",
-          inform: "Lorem Ipsum Dollar",
-        }}
-      />
+      {data?.map((i) => (
+        <Following user={i} />
+      ))}
     </div>
   );
 };
