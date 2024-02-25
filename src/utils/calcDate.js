2732284f519e762eaff9d49b25a2bcb689492e82
six-standard <dbrrlwns1127@gmail.com>
2024-02-25 12:54:37 +0900

    fix: 오늘 작성한 글이 어제로 표시되던 오류 수정

--- src/utils/calcDate.js
+++ src/utils/calcDate.js
@@ -6,8 +6,10 @@ export const calcDate = (date) => {
   let diff = date1.getTime() - date2.getTime();
   diff = Math.ceil(diff / (1000 * 60 * 60 * 24));
   if (diff === 1) {
+    return "오늘";
+  } else if (diff === 2) {
     return "어제";
-  } else if (diff < 7) {
+  } else if (diff < 8) {
     return `${diff}일 전`;
   } else {
     return date.split(" ")[0];
