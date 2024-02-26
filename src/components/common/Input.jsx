98f6bb3e050b42b77e46ab26bb1977448e812751
six-standard <dbrrlwns1127@gmail.com>
2024-02-26 12:39:49 +0900

    chore: input 컴포넌트 keydown 옵션 추가

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -5,12 +5,14 @@ export default function Input({
   id,
   value,
   action,
+  keyDown,
 }) {
   return (
     <input
-      className={`${className} w-full p-2 bg-neutral-100 placeholder:text-neutral-400 dark:bg-neutral-800 dark:placeholder:text-neutral-600 dark:text-neutral-50 rounded-md`}
+      className={`${className} p-2 bg-neutral-100 placeholder:text-neutral-400 dark:bg-neutral-800 dark:placeholder:text-neutral-600 dark:text-neutral-50 rounded-md`}
       placeholder={placeholder}
       onChange={action}
+      onKeyDown={keyDown}
       type={type}
       value={value}
       id={id}
