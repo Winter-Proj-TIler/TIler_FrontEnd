80b79f5d23b38111891c7e77dd0df2dd47464e94
six-standard <dbrrlwns1127@gmail.com>
2024-02-26 12:39:19 +0900

    chore: button 컴포넌트 ID 추가

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,6 +1,7 @@
-export default function Button({ children, onClick, className }) {
+export default function Button({ children, onClick, className, id }) {
   return (
     <button
+      id={id}
       className={`${className} flex gap-1 box-content px-2 rounded-lg border-[1px] border-neutral-900 dark:border-neutral-100 dark:text-neutral-100`}
       onClick={onClick}
     >
