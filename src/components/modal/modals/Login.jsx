2a11b36b510c555f5460f2280aae8014a2c610b6
six-standard <dbrrlwns1127@gmail.com>
2024-02-26 12:41:34 +0900

    feat: 로그인/회원가입 컴포넌트 기능 완성

--- src/components/modal/modals/Login.jsx
+++ src/components/modal/modals/Login.jsx
@@ -1,25 +1,98 @@
+import { useState } from "react";
 import { Google, Github } from "../../../assets";
 import useModal from "../../../hooks/useModal";
 import Input from "../../common/Input";
+import { findPW, googleAuth, login, searchUser } from "../../../api/user";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+import useUser from "../../../hooks/useUser";
 
 export const Login = () => {
-  const { open } = useModal();
+  const { open, close } = useModal();
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+  const { add } = useUser();
 
   const handleRegister = () => {
     open("register");
+  };
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.id]: e.target.value });
+  };
+
+  const handleFind = () => {
+    if (data.email) {
+      findPassword();
+    } else {
+      toast.error("이메일을 다시 한번 확인해 주세요");
+    }
+  };
+
+  const { mutate: findPassword } = useMutation({
+    mutationFn: () => findPW(data.email),
+    onSuccess: () => {
+      toast.success("이메일로 비밀번호가 발송되었습니다.");
+    },
+  });
+
+  const handleLogin = () => {
+    let load = toast.loading("로그인중입니다..");
+    login(data).then((res) => {
+      if (!!!res.code) {
+        const { data } = res.data;
+        localStorage.setItem("accessToken", data.accessToken);
+        localStorage.setItem("refreshToken", data.refreshToken);
+        localStorage.setItem("userID", data.userId);
+        searchUser(data.userId).then((res) => {
+          const { user } = res.data.data;
+          add(user);
+          close();
+          toast.update(load, {
+            render: "로그인되었습니다",
+            type: "success",
+            isLoading: false,
+            autoClose: 1000,
+          });
+        });
+      } else {
+        toast.update(load, {
+          render: "이메일 또는 비밀번호를 다시 확인해주세요",
+          type: "error",
+          isLoading: false,
+          autoClose: 1000,
+        });
+      }
+    });
   };
 
   return (
     <div className="grid grid-rows-[1fr_3fr_3fr] gap-2 place-content-center place-items-center">
       <h1 className="text-2xl dark:text-neutral-50">Login</h1>
       <div className="w-96 flex flex-col gap-6">
-        <Input placeholder="이메일" />
-        <Input type="password" placeholder="비밀번호" />
+        <Input
+          placeholder="이메일"
+          id="email"
+          value={data.email}
+          action={handleChange}
+        />
+        <Input
+          type="password"
+          placeholder="비밀번호"
+          value={data.password}
+          action={handleChange}
+          id="password"
+        />
         <div className="flex justify-between items-center">
           <div>
             <h1 className="text-xs dark:text-neutral-50">
               비밀번호를 잊으셨나요?
-              <span className="cursor-pointer text-neutral-400 ml-1">
+              <span
+                className="cursor-pointer text-neutral-400 ml-1"
+                onClick={handleFind}
+              >
                 비밀번호 찾기
               </span>
             </h1>
@@ -33,19 +106,25 @@ export const Login = () => {
               </span>
             </h1>
           </div>
-          <button className="px-8 py-2 bg-neutral-100 dark:bg-neutral-800 dark:text-neutral-50 rounded-md">
+          <button
+            className="px-8 py-2 bg-neutral-100 dark:bg-neutral-800 dark:text-neutral-50 rounded-md"
+            onClick={handleLogin}
+          >
             로그인
           </button>
         </div>
       </div>
       <div className="flex flex-col gap-4 w-80 p-3 box-content">
         <div className="flex justify-center items-center relative p-5">
-          <h1 className="absolute bg-neutral-50 dark:bg-neutral-950 dark:text-neutral-50 z-10 px-2">
+          <h1 className="absolute bg-[#ffffff] dark:bg-neutral-950 dark:text-neutral-50 z-10 px-2">
             또는
           </h1>
           <hr className="absolute w-full bg-gray-900 dark:bg-neutral-50 h-[1px]" />
         </div>
-        <div className="p-3 bg-neutral-100 dark:bg-neutral-700 rounded-md cursor-pointer flex justify-center items-center relative">
+        <div
+          onClick={googleAuth}
+          className="p-3 bg-neutral-100 dark:bg-neutral-700 rounded-md cursor-pointer flex justify-center items-center relative"
+        >
           <img
             src={Google}
             alt=""
--- src/components/modal/modals/Register.jsx
+++ src/components/modal/modals/Register.jsx
@@ -1,5 +1,9 @@
+import { useState } from "react";
 import useModal from "../../../hooks/useModal";
 import Input from "../../common/Input";
+import { signUp } from "../../../api/user";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-toastify";
 
 export const Register = () => {
   const { open } = useModal();
@@ -8,14 +12,59 @@ export const Register = () => {
     open("login");
   };
 
+  const [data, setData] = useState({
+    email: "",
+    username: "",
+    password: "",
+    passCheck: "",
+  });
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.id]: e.target.value });
+  };
+
+  const { mutate: handleRegister } = useMutation({
+    mutationFn: () => signUp(data),
+    onSettled: (res) => {
+      if (!!!res.code) {
+        toast.success("성공적으로 가입되었습니다.");
+        handleLogin();
+      } else {
+        toast.error("이미 존재하는 계정입니다.");
+      }
+    },
+  });
+
   return (
     <div className="grid grid-rows-[1fr_3fr] gap-2 place-content-center place-items-center">
       <h1 className="text-2xl dark:text-neutral-50">Register</h1>
       <div className="w-96 flex flex-col gap-6">
-        <Input placeholder="이메일" />
-        <Input placeholder="계정명" />
-        <Input type="password" placeholder="비밀번호" />
-        <Input type="password" placeholder="비밀번호 확인" />
+        <Input
+          placeholder="이메일"
+          id="email"
+          value={data.email}
+          action={handleChange}
+        />
+        <Input
+          placeholder="계정명"
+          id="username"
+          value={data.username}
+          action={handleChange}
+        />
+        <Input
+          type="password"
+          placeholder="비밀번호"
+          value={data.password}
+          id="password"
+          action={handleChange}
+        />
+        <Input
+          type="password"
+          placeholder="비밀번호 확인"
+          value={data.passCheck}
+          id="passCheck"
+          action={handleChange}
+        />
         <div className="flex justify-between items-center">
           <h1 className="text-xs dark:text-neutral-50">
             이미 회원이신가요?
@@ -26,7 +75,13 @@ export const Register = () => {
               로그인
             </span>
           </h1>
-          <button className="px-8 py-2 bg-neutral-100 dark:bg-neutral-800 dark:text-neutral-50 rounded-md">
+          <button
+            disabled={
+              !(data.password === data.passCheck && data.password.length)
+            }
+            className="px-8 py-2 bg-neutral-100 dark:bg-neutral-800 dark:text-neutral-50 rounded-md"
+            onClick={handleRegister}
+          >
             회원가입
           </button>
         </div>
